a9be8d9524292d8b91839d3a02988f48fc6366eb
Speedy <148458995+speedyownjoo@users.noreply.github.com>
2025-06-09 09:42:39 -0400

    add test

---
+++ test/test_main.py
@@ -0,0 +1,20 @@
+import unittest
+
+from main import main
+
+
+class TestIOChainMain(unittest.TestCase):
+    def test_main(self):
+        # setup
+
+        # execute
+        actual = main()
+
+        # assess
+        self.assertTrue(True)
+
+        # teardown
+
+
+if __name__ == '__main__':
+    unittest.main()
